21bea4d33501e9d929f731a003bca562a92b48d5
Gökhan Karaman <gmkaraman@msn.com>
2026-03-09 05:06:03 +0300

    news added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./content/**/*.{html,md}",
+    "./layouts/**/*.{html,js}",
+    "./assets/**/*.{js,css}",
+    "./themes/hugo-news/layouts/**/*.{html,js}",
+    "./themes/hugo-news/assets/**/*.{js,css}",
+    "./themes/hugo-news/exampleSite/**/*.{html,md,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
